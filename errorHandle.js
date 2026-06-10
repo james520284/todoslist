806551f13546e1e2caeb414d03b0f727bba516a5
james520284 <james520284@gmail.com>
2026-06-10 13:49:05 +0800

    修正

---
+++ errorHandle.js
@@ -0,0 +1,18 @@
+
+const errorHandle= (res,reason)=>{
+
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+        'Content-Type': 'application/json'
+    }
+    res.writeHead(400,headers)
+    res.write(JSON.stringify({
+        "status":'false',
+        "message":reason,
+    }))
+    res.end();
+}
+
+export default errorHandle;
